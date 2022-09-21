08025a3348c78ce6614d39845974a762cf5abfc0
biko-73 <57875905+biko-73@users.noreply.github.com>
2022-09-21 13:36:57 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -12,8 +12,8 @@
 
 PACKAGE_DIR='OsCam_EMU/main'
 
-MY_IPK="enigma2-plugin-softcams-oscam_11.711-emu-r798_all.ipk"
-MY_DEB="enigma2-plugin-softcams-oscam-osdreambox_11.711-emu-r798_all.deb"
+MY_IPK="enigma2-plugin-softcams-oscam_11.713-emu-r798_all.ipk"
+MY_DEB="enigma2-plugin-softcams-oscam-osdreambox_11.713-emu-r798_all.deb"
 
 ########################################################################################################################
 # Auto ... Do not change
